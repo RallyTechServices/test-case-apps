01b30f10d650669d97e06d1d4c62de0454b99672
Kristy Corkan <kcorkan@gmail.com>
2017-06-05 07:14:16 -0600

    added tags to filters

---
+++ last-verdict-by-timebox/src/javascript/utils/overrides.js
@@ -0,0 +1,7 @@
+Ext.override(Rally.ui.inlinefilter.PropertyFieldComboBox, {
+    /**
+     * @cfg {String[]} whiteListFields
+     * field names that should be included from the filter row field combobox
+     */
+    defaultWhiteListFields: ['Milestones','Tags']
+});
